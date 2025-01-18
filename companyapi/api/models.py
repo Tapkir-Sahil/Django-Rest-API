368e636fd8ae5d0663c134236f89cc4d719f5a59
sahil-tapkir <sahiltapkir07@gmail.com>
2025-01-18 19:04:01 +0530

    Add Employee model and serializers, update API views and URLs

--- companyapi/api/models.py
+++ companyapi/api/models.py
@@ -16,4 +16,20 @@ class Company(models.Model):
     added_date=models.DateTimeField(auto_now=True)
     active=models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.name
 #Employee Model 
+
+class Employee(models.Model):
+    name=models.CharField(max_length=100)
+    email_id=models.CharField(max_length=50)
+    address=models.CharField(max_length=200)
+    phone=models.CharField(max_length=10)
+    about=models.TextField(max_length=100)
+    position=models.CharField(max_length=50,choices=(
+        ('MG','Manager'),
+        ('SDE','Software Developer'),
+        ('PL','Project Lead')
+    ))
+    
+    company=models.ForeignKey(Company,on_delete=models.CASCADE)
--- companyapi/api/views.py
+++ companyapi/api/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from api.models import Company
-from api.serializers import CompanySerializers
+from api.models import Company,Employee
+from api.serializers import CompanySerializers,EmployeeSerializers
 # Create your views here.
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset=Company.objects.all()
     serializer_class=CompanySerializers
+
+    def employees(self,request,pk=None):
+        
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset=Employee.objects.all()
+    serializer_class=EmployeeSerializers
