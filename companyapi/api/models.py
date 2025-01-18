3b8da390c54423a56c2c3ee2b7dd5f8e102a6409
sahil-tapkir <sahiltapkir07@gmail.com>
2025-01-18 13:12:25 +0530

    Add initial files and structure for companyapi and tutorial packages

---
+++ companyapi/api/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+#Company models created here
+
+class Company(models.Model):
+    company_id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=50)
+    location=models.CharField(max_length=50)
+    about=models.TextField()
+    type=models.CharField(max_length=100,choices=(('IT','IT'),
+                                                  ('NON IT','NON IT'),
+                                                  ('MP','MP')
+                                                  ))
+    added_date=models.DateTimeField(auto_now=True)
+    active=models.BooleanField(default=True)
+
+#Employee Model 
